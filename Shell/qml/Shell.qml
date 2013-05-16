8ff3c3d6d50529d8c4f88464765abcd8c3b9fe92
Penk Chen <penkia@gmail.com>
2013-05-16 21:33:31 +0300

    Started working on custom input element

--- Shell/qml/Shell.qml
+++ Shell/qml/Shell.qml
@@ -204,6 +204,11 @@ Item {
                         }
                         break;
                     } 
+                    case 'select': {
+                        console.log(data.text);
+                        console.log(data.value);
+                        break;
+                    }
                 }
             }
             //experimental.userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3"
--- Shell/qml/userscript.js
+++ Shell/qml/userscript.js
@@ -1,6 +1,7 @@
 document.documentElement.addEventListener('click', (function(e) {
     var node = e.target;
     while(node) {
+        // hook for Open in New Tab (link with target)
         if (node.tagName === 'A') {
             var link = new Object
             link.type = 'link'
@@ -9,15 +10,31 @@ document.documentElement.addEventListener('click', (function(e) {
             link.href = node.getAttribute('href');
             navigator.qt.postMessage( JSON.stringify(link) );
         }
+
+        // custom dialog for select input element
+        if (node.tagName === 'SELECT') {
+            var select = new Object; 
+            select.type = 'select';
+            select.text = [''];
+            select.value = [''];
+            for (var i=0; i < node.options.length; i++) {
+                select.text.push(node.options[i].text);
+                select.value.push(node.options[i].value); 
+            }
+            navigator.qt.postMessage( JSON.stringify(select));
+        }
+
         node = node.parentNode;
     }
 }), true);
 
+
+// FIXME: experiementing on tap and hold 
 var hold;
 function longPressed(x, y) { navigator.qt.postMessage('longpressed: '+x+', '+y) }
 window.document.addEventListener('mousedown', (function(e) {
 //    hold = setTimeout(longPressed, 800, e.clientX, e.clientY); 
-    navigator.qt.postMessage('mousedown: '+e.clientX+', '+e.clientY)
+//    navigator.qt.postMessage('mousedown: '+e.clientX+', '+e.clientY)
 }), true);
 window.document.addEventListener('mouseup', (function() {
     clearTimeout(hold); 
